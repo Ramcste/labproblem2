467640f76ae68d744239f643cd984aad13fbca99
ramcste <lucky.guyram@gmail.com>
2015-04-25 10:02:44 +0600

    lab problem 2

---
+++ Problem2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+               
+                if (i%2 == 0)
+                {
+                    Console.Write(i+" ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
